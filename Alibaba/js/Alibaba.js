4799e6d55831627294888ed964e31b8789b04b4a
cslgjiangjinjian <2582253358@qq.com>
2013-10-12 21:35:45 +0800

    update the alibaba.com
    update the alibaba ,do the point

--- Alibaba/js/Alibaba.js
+++ Alibaba/js/Alibaba.js
@@ -2,6 +2,8 @@ $(function(){
 
 	initEvents();
 		initPosition();
+
+		setInterval("add();",5000);
 }); 
 
 
@@ -12,7 +14,7 @@ $(function(){
 function initPosition(){
 
 $.each($(".tbmenu"),function(i){
- $(this).css("left",i * 10 + "px");
+ $(this).css("right",i * 30 + "px");
 });
 
 }
@@ -35,4 +37,37 @@ function initEvents(){
 	},function(){
 		$(this).css("background-position","0 0");
 	});
+
+	$("#keyword").keyup(function(){
+		var val = $("#keyword").val();
+		if (val == "") {
+			$("#keyword").css("background-image",'url("css/images/20130930214745.jpg")');
+
+		}else{
+			$("#keyword").css("background-image","none");
+		}
+	});
+	
+
+
 }
+	var i=0;
+ 	function add(){
+ 		i++;
+ 	
+	$(".yellow").css("background-position","0 -150px");
+	if (i!=4) {
+
+		$("#middlepic").css("background-image",'url("css/images/'+i+'.jpg")');
+		$(".yellow").each(function(){
+			if (i ==$(this).attr("txt")){
+				$(this).css("background-position","0 -165px");
+			};
+		})
+	}else{
+		i=1;
+		$("#yellow"+i).css("background-position","0 -150px");
+		$("#middlepic").css("background-image",'url("css/images/'+i+'.jpg")');
+		$("#yellow"+i).css("background-position","0 -165px");
+	}
+ 	}
